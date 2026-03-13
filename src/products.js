0e6b1fe33135e0b8cde854af0a66dc6c8b205a25
Hong Nhung <hongnhung2001thd@gmail.com>
2026-03-13 12:37:37 +0900

    Deploy

--- src/products.js
+++ src/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    image: "./images/airpods.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
@@ -15,7 +15,7 @@ const products = [
   {
     _id: "2",
     name: "iPhone 11 Pro 256GB Memory",
-    image: "/images/phone.jpg",
+    image: "./images/phone.webp",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
@@ -28,7 +28,7 @@ const products = [
   {
     _id: "3",
     name: "Cannon EOS 80D DSLR Camera",
-    image: "/images/camera.jpg",
+    image: "/images/camera.webp",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
     brand: "Cannon",
